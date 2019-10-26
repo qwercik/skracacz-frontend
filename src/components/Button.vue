03d47cfc7512dfad361d3ff4befd9ed14c96f10d
qwercik <erykandrzejewski@gmail.com>
2019-10-26 19:39:25 +0200

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="$emit('click', $emit)">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .button {
+    color: #303030;
+    background: #EEEEEE;
+    border: 1px solid #202020;
+
+    padding: 10px;
+    transition: 0.6s;
+  }
+
+  .button:hover,
+  .button:focus {
+    background: #55F352;
+    transition: 0.6s
+  }
+</style>
