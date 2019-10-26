6ef750c6c6b1a84a4809d2806eda189ccd374e63
qwercik <erykandrzejewski@gmail.com>
2019-10-26 20:54:43 +0200

    Add Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    v-bind="$attrs"
+    @input="$emit('input', $event.target.value)"
+    class="link"
+  />
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<style>
+  .link {
+    padding: 10px;
+    border: 1px solid #202020;
+  }
+</style>
