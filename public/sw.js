8e6bd3fe52f0ab48421c862a8e6e7f8560e104ff
qwercik <erykandrzejewski@gmail.com>
2019-10-27 16:05:54 +0100

    Remove vue icons files

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,8 @@
-var cacheName = 'hello-pwa'
+var cacheName = 'skracacz-frontend'
 var filesToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/main.js'
 ]
 
 /* Start the service worker and cache all of the app's content */
